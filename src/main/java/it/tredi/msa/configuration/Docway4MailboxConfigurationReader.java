9635f4fdc4575e5359aed29125c61aee540da584
sstagni <sstagni@3di.it>
2018-01-25 15:53:52 +0100

    update

--- src/main/java/it/tredi/msa/configuration/Docway4MailboxConfigurationReader.java
+++ src/main/java/it/tredi/msa/configuration/Docway4MailboxConfigurationReader.java
@@ -15,65 +15,85 @@ public class Docway4MailboxConfigurationReader extends MailboxConfigurationReade
 	public String getHost() {
 		return host;
 	}
+	
 	public void setHost(String host) {
 		this.host = host;
 	}
+	
 	public int getPort() {
 		return port;
 	}
+	
 	public void setPort(String port) {
 		this.setPort(Integer.parseInt(port));
 	}
+	
 	public void setPort(int port) {
 		this.port = port;
 	}
+	
 	public String getUser() {
 		return user;
 	}
+	
 	public void setUser(String user) {
 		this.user = user;
 	}
+	
 	public String getPassword() {
 		return password;
 	}
+	
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
 	public String getDb() {
 		return db;
 	}
+	
 	public void setDb(String db) {
 		this.db = db;
 	}
+	
 	public boolean getQuery() {
 		return query;
 	}
+	
 	public void setQuery(boolean query) {
 		this.query = query;
 	}
+	
 	public void setQuery(String query) {
 		this.setQuery(Boolean.parseBoolean(query));
 	}
+	
 	public boolean getQueryInterop() {
 		return queryInterop;
 	}
+	
 	public void setQueryInterop(boolean queryInterop) {
 		this.queryInterop = queryInterop;
 	}
+	
 	public void setQueryInterop(String queryInterop) {
 		this.setQueryInterop(Boolean.parseBoolean(queryInterop));
 	}
+	
 	public String getXPathInfo() {
 		return XPathInfo;
 	}
+	
 	public void setXPathInfo(String xPathInfo) {
 		this.XPathInfo = xPathInfo;
 	}
+	
 	@Override
 	public Object getRawData() {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
 	@Override
 	public Docway4MailboxConfigurationReader readConfiguration() {
 		// TODO Auto-generated method stub
