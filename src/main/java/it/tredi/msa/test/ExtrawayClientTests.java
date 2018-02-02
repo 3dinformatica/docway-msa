5a2970cf4efee5448f269f982bd9081b15c97fc8
marsidac <marsidac1@gmail.com>
2018-02-02 14:17:06 +0100

    Fatto extraway.properties e aggiunto method static in ExtrawayClintTests

--- src/main/java/it/tredi/msa/test/ExtrawayClientTests.java
+++ src/main/java/it/tredi/msa/test/ExtrawayClientTests.java
@@ -6,6 +6,9 @@ import org.junit.runners.MethodSorters;
 import it.tredi.extraway.ExtrawayClient;
 import it.tredi.msa.Services;
 
+import java.io.IOException;
+import java.util.Properties;
+
 import org.dom4j.Document;
 import org.junit.Assert;
 import org.junit.FixMethodOrder;
@@ -15,18 +18,40 @@ public class ExtrawayClientTests {
 	
 	private static ExtrawayClient client; 
 	
+	private static String host;
+	private static int port;
+	private static String user;
+	private static String password;
+	private static String db;
+	private static String query;
+	
+	static {
+		Properties properties = new Properties();
+		try {
+			properties.load(ExtrawayClientTests.class.getResourceAsStream("extraway.properties"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		host = properties.getProperty("extraway.host");
+		port = Integer.parseInt(properties.getProperty("extraway.port"));
+		user = properties.getProperty("extraway.user");
+		password = properties.getProperty("extraway.password");
+		db = properties.getProperty("extraway.db");
+		query = properties.getProperty("extraway.query");
+	}
 	@Test
 	public void test_001_connect() throws Exception {
-		client = new ExtrawayClient("docway-test.bo.priv", 4859, "acl", "lettore", "reader");
+		client = new ExtrawayClient(host, port, db, user, password);
 		client.connect();
 	}
 
 	@Test
 	public void test_003_search() throws Exception {
-		int count = client.search("[casellapostaelettronica_interop]=\"no\"");
+		int count = client.search(query);
 		Assert.assertTrue(count > 0);
 	}
-
+	
 	@Test
 	public void test_004_loadDocument() throws Exception {
 		Document document = client.loadDocByQueryResult(0);
